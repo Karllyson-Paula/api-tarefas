5c77b72240160ee02cf2b57cfda53e629614e0e6
Karllyson-Paula <karllyson.eduardo@gmail.com>
2026-07-12 19:40:07 -0300

    feat: adiciona CORS para o frontend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const tarefasRouter = require('./routes/tarefas')
 const authRouter = require('./routes/auth')
 const logger = require('./middlewares/logger')
@@ -6,6 +7,10 @@ const erros = require('./middlewares/erros')
 
 const app = express()
 
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000']
+}))
 
 app.use(express.json())
 app.use(logger)
